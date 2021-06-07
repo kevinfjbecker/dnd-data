c0332f8444cefe610c66362a2667b9be9823d5ee
Kevin Becker <kevinfjbecker@gmail.com>
2021-06-07 20:31:49 +0200

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://cdn.jsdelivr.net/gh/kevinfjbecker/dnd-data/randomtableparser.js';
+    document.body.appendChild(script);
+}());
