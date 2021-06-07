5f782f0d6b340bcda61f7c254d2aaf7991ad0cd2
Kevin Becker <kevinfjbecker@gmail.com>
2021-06-08 00:34:09 +0200

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-(()=>{const link=document.createElement('script');link.src='https://cdn.jsdelivr.net/gh/kevinfjbecker/dnd-data/bootloader.js';document.head.appendChild(link);})();
+(()=>{const link=document.createElement('script');link.src='https://cdn.jsdelivr.net/gh/kevinfjbecker/dnd-data@0.0.2/bootloader.js';document.head.appendChild(link);})();
